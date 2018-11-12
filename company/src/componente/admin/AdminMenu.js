57284313a51a8efe089aae9a124afd83ce2e9423
HemersonRafael <rafaelpontes1995@gmail.com>
2018-11-12 17:57:58 -0300

    add admin

--- company/src/componente/admin/AdminMenu.js
+++ company/src/componente/admin/AdminMenu.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import {Router} from 'react-router-dom'
 
 const AdminMenu = props => {
     return (
         <div>
             <h3>Menu Administrativo</h3>
             <div className="list-group">
-                <a href="#" className="list-group-item list-group-item-action active">
+                <a className="list-group-item list-group-item-action active">
                     Selecione uma opção
                 </a>
-                <a href="#" className="list-group-item list-group-item-action">Portifolio</a>
+                <a href="admin/portfolio" className="list-group-item list-group-item-action">Portifolio</a>
                 
             </div>
         </div>
